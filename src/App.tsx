faa0244daa1ddf5a9853841655724eb31ff422a3
Alexander Brown <alex@macbook-pro.mynetworksettings.com>
2022-11-30 23:32:21 -0500

    set up components folder & tested out MUI

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import CssBaseline from '@mui/material/CssBaseline'
 import {Helmet} from "react-helmet";
+import HelloWorld from './components/helloWorld';
 
 function App() {
   return (
@@ -12,18 +13,7 @@ function App() {
       </Helmet>
       <CssBaseline />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HelloWorld />
       </header>
     </div>
   );
