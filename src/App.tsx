829ec9410ed2cea43d1bd0b1a612b0612819c4bf
Alexander Brown <alex@macbook-pro.mynetworksettings.com>
2022-12-03 13:27:04 -0500

    Setup & ran eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import CssBaseline from "@mui/material/CssBaseline";
-import { Helmet } from "react-helmet";
-import HelloWorld from "./components/helloWorld";
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import CssBaseline from '@mui/material/CssBaseline'
+import { Helmet } from 'react-helmet'
+import HelloWorld from './components/helloWorld'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Helmet>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Helmet>
       <CssBaseline />
-      <header className="App-header">
+      <header className='App-header'>
         <HelloWorld />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/helloWorld.tsx
+++ src/components/helloWorld.tsx
@@ -1,12 +1,12 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 
 const HelloWorld = () => {
   return (
     <Box>
-      <Typography variant="h1">Hello, World!</Typography>
+      <Typography variant='h1'>Hello, World!</Typography>
     </Box>
-  );
-};
+  )
+}
 
-export default HelloWorld;
+export default HelloWorld
